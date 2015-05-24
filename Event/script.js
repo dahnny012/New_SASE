176e2372dc57bf8180c2cc1ad262057e8f678849
Danh Nguyen <filler012@gmail.com>
2015-05-24 09:56:01 +0000

    react is pretty cool

--- Event/script.js
+++ Event/script.js
@@ -1,1 +1,39 @@
 moment().format();
+
+Event = React.createClass({
+    getInitialState: function() {
+        return {
+            name: "",
+            date: "",
+            location: "",
+            time: "",
+            description: "",
+            fb: ""
+        };
+    },
+
+    componentDidMount: function() {
+        $.get(this.props.source, function(result) {
+            var result = JSON.parse(result).events[0];
+            if (this.isMounted()) {
+                this.setState({
+                    name: result.name,
+                    date: result.data,
+                    location: result.location,
+                    time: result.time,
+                    description: result.description,
+                    fb: result.fb
+                });
+            }
+        }.bind(this));
+    },
+
+    render: function() {
+        return ( <div>{this.state.name}</div>);
+}
+});
+
+
+
+React.render( <Event source = "https://new-sase-dahnny012.c9.io/API/event.php"/> 
+, document.getElementById("events"));
